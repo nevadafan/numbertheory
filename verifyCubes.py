b58931dc2b638b9a33ee7948f5064067e30ce83e
nevadafan <anand.manikutty@gmail.com>
2016-06-10 00:57:49 -0700

    Create verifyCubes.py

---
+++ verifyCubes.py
@@ -0,0 +1,19 @@
+# verify that the sum of the first n cubes is equal to the square of the first n numbers
+
+def verify_until(num_iterations):
+
+    sum_integers = 0
+    sum_cubes = 0
+
+    for index in range(num_iterations):
+        number = index + 1;
+        sum_integers += number;
+        sum_cubes += (number*number*number)
+        print (str(index+1) + ". SUM OF ALL INTEGERS UNTIL THIS POINT =      " + str(sum_integers) + "; squared = " + str(sum_integers*sum_integers) + " -> " + str(sum_cubes))
+        diff = sum_cubes - (sum_integers * sum_integers)
+        if(diff != 0):
+            print "found an anomaly"
+            print (index + 1)
+            break;
+
+verify_until(1000);

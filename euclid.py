bebab439e91d3669d2d2365bfa7e9f28308804be
nevadafan <anand.manikutty@gmail.com>
2016-06-10 01:18:49 -0700

    Update euclid.py

--- euclid.py
+++ euclid.py
@@ -1,6 +1,8 @@
 def an_and_gcd(a, b):
   '''
-  Euclid's algorithm for gcd
+  Euclid's algorithm for gcd of two numbers
+  >>> an_and_gcd(12,4)
+  4
 
   '''
   j, j1 = 1, 0
@@ -10,4 +12,4 @@ def an_and_gcd(a, b):
     a, b = b, r
     j, j1 = j1, j - q * j1
     k, k1 = k1, k - q * k1
-  return (j, k, a) if a > 0 else (-j, -k, -a)
+  return ((j, k, a) if a > 0 else (-j, -k, -a))

bc77eaa2ebb48c12bce2298288c7893965dfc2d2
nevadafan <anand.manikutty@gmail.com>
2016-06-10 01:16:53 -0700

    Create euclid.py

---
+++ euclid.py
@@ -0,0 +1,13 @@
+def an_and_gcd(a, b):
+  '''
+  Euclid's algorithm for gcd
+
+  '''
+  j, j1 = 1, 0
+  k, k1 = 0, 1
+  while b:
+    q, r = divmod(a,  b)
+    a, b = b, r
+    j, j1 = j1, j - q * j1
+    k, k1 = k1, k - q * k1
+  return (j, k, a) if a > 0 else (-j, -k, -a)
